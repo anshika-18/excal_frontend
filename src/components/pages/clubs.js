f1970712c39052da42df3bba2547ba4a323d141e
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-11-25 18:33:48 +0530

    API CALLS Function

--- src/components/pages/clubs.js
+++ src/components/pages/clubs.js
@@ -1,8 +1,23 @@
-import React,{Component} from 'react'
+import axios from "axios";
+import React, { Component } from 'react'
 
-export default class notification extends Component{
-    render(){
-        return(
+
+export default class notification extends Component {
+    componentDidMount() {
+        console.log("hello");
+        axios
+            .get("http://localhost:9000/clubs")
+            .then((response) => {
+                console.log(response.data);
+                // this.setState({ contestants: response.data });
+            })
+            .catch((error) => {
+                console.log(error);
+                alert("Server Error");
+            });
+    }
+    render() {
+        return (
             <div>
                 <p>COMING SOON...</p>
             </div>
